b0dea5df2de4e21931d5b216cda7026d2ae0ccb2
Yusuke Miyazaki <ysk1180@gmail.com>
2020-07-10 08:32:25 +0900

    add self-yield

---
+++ self-yield.rb
@@ -0,0 +1,42 @@
+# よくあるコード
+class PC
+  def initialize(name, os, price)
+    @name = name
+    @os = os
+    @price = price
+  end
+end
+
+mac_book_air = PC.new('mac book air', 'mac', 150_000)
+p mac_book_air #-> #<PC:0x00007fcf05863158 @name="mac book air", @os="mac", @price=150000>
+
+# 自己yield活用パターン
+class PC
+  attr_writer :name, :os, :price
+
+  def initialize
+    yield self
+  end
+end
+
+mac_book_pro = PC.new do |pc|
+  pc.name = 'mac book pro'
+  pc.os = 'mac'
+  pc.price = 200_000
+end
+p mac_book_pro #-> #<PC:0x00007f8015017128 @name="mac book pro", @os="mac", @price=200000>
+
+# どっちも対応できるパターン
+class PC
+  attr_writer :name, :os, :price
+
+  def initialize(name = nil, os = nil, price = nil)
+    if block_given?
+      yield self
+    else
+      @name = name
+      @os = os
+      @price = price
+    end
+  end
+end
